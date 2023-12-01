5fc1b3688060cb0f6d8c3883ce935d459414ac59
spysabbir <sabbirahammedsovon@gmail.com>
2023-12-01 10:55:28 +0600

    Error page design

--- resources/views/errors/401.blade.php
+++ resources/views/errors/401.blade.php
@@ -1,5 +1,71 @@
-@extends('errors::minimal')
+@php
+    $default_setting = App\Models\Default_setting::first();
+@endphp
+@extends('errors::layout')
 
-@section('title', __('Unauthorized'))
-@section('code', '401')
-@section('message', __('Unauthorized'))
+@section('title_bar')
+401 - Unauthorized
+@endsection
+
+@section('body_content')
+<div class="row clearfix">
+    <div class="col-12">
+        <nav class="navbar navbar-expand-lg">
+            <a class="navbar-brand" href="javascript:void(0);">
+                <img src="{{asset('uploads/default_photo')}}/{{$default_setting->logo_photo}}" class="d-inline-block align-top mr-2" alt="">
+            </a>
+        </nav>
+    </div>
+    <div class="col-lg-8">
+        <div class="auth_detail">
+            <h2 class="text-monospace">
+                Everything<br> you need for
+                <div id="carouselExampleControls" class="carousel vert slide" data-ride="carousel" data-interval="1500">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">your Admin</div>
+                        <div class="carousel-item">your Project</div>
+                        <div class="carousel-item">your Dashboard</div>
+                        <div class="carousel-item">your Application</div>
+                        <div class="carousel-item">your Client</div>
+                    </div>
+                </div>
+            </h2>
+            <p>Please enter your email address below to receive instructions for resetting password.</p>
+            <ul class="social-links list-unstyled">
+                <li>
+                    <a class="btn btn-default" target="_blank" href="{{$default_setting->facebook_link}}" data-toggle="tooltip" data-placement="top" title="Facebook"><i class="fa fa-facebook"></i></a>
+                </li>
+                <li>
+                    <a class="btn btn-default" target="_blank" href="{{$default_setting->twitter_link}}" data-toggle="tooltip" data-placement="top" title="Twitter"><i class="fa fa-twitter"></i></a>
+                </li>
+                <li>
+                    <a class="btn btn-default" target="_blank" href="{{$default_setting->instagram_link}}" data-toggle="tooltip" data-placement="top" title="Instagram"><i class="fa fa-instagram"></i></a>
+                </li>
+                <li>
+                    <a class="btn btn-default" target="_blank" href="{{$default_setting->linkedin_link}}" data-toggle="tooltip" data-placement="top" title="Linkedin"><i class="fa fa-linkedin"></i></a>
+                </li>
+                <li>
+                    <a class="btn btn-default" target="_blank" href="{{$default_setting->youtube_link}}" data-toggle="tooltip" data-placement="top" title="Youtube"><i class="fa fa-youtube"></i></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="col-lg-4">
+        <div class="card">
+            <div class="header">
+                <h3>
+                    <span class="clearfix title">
+                        <span class="number left">Error </span><span class="text">401 <br/>Unauthorized!</span>
+                    </span>
+                </h3>
+            </div>
+            <div class="body">
+                <p>You don't have permission to access / on this server.</p>
+                <div class="margin-top-30">
+                    <a href="javascript:history.go(-1)" class="btn btn-default btn-block"><i class="fa fa-arrow-left"></i> <span>Go Back</span></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- resources/views/errors/layout.blade.php
+++ resources/views/errors/layout.blade.php
@@ -1,53 +1,41 @@
-<!DOCTYPE html>
+@php
+    $default_setting = App\Models\Default_setting::first();
+@endphp
+<!doctype html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>@yield('title')</title>
+<head>
+    <title>:: {{ env('APP_NAME') }} :: Admin :: @yield('title_bar')</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <meta name="description" content="{{ $default_setting->app_name }} Admin Template">
+    <meta name="author" content="Spy IT Firm, www.spyitfirm.com">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+    <link rel="icon" href="{{asset('uploads/default_photo')}}/{{$default_setting->favicon}}" type="image/x-icon">
+    <!-- VENDOR CSS -->
+    <link rel="stylesheet" href="{{asset('admin')}}/vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{asset('admin')}}/vendor/font-awesome/css/font-awesome.min.css">
 
-            .full-height {
-                height: 100vh;
-            }
+    <!-- MAIN CSS -->
+    <link rel="stylesheet" href="{{asset('admin')}}/css/main.css">
+    <link rel="stylesheet" href="{{asset('admin')}}/css/color_skins.css">
+</head>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+<body class="theme-cyan">
 
-            .position-ref {
-                position: relative;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 36px;
-                padding: 20px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            <div class="content">
-                <div class="title">
-                    @yield('message')
-                </div>
-            </div>
+    <!-- WRAPPER -->
+    <div id="wrapper" class="auth-main">
+        <div class="container">
+            @yield('body_content')
         </div>
-    </body>
+    </div>
+    <!-- END WRAPPER -->
+
+    <script src="{{asset('admin')}}/bundles/libscripts.bundle.js"></script>
+    <script src="{{asset('admin')}}/bundles/vendorscripts.bundle.js"></script>
+
+    <script src="{{asset('admin')}}/bundles/mainscripts.bundle.js"></script>
+</body>
+
 </html>
