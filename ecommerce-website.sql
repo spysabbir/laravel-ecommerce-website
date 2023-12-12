f48c98a0e514098796a6cc54d0c6086be229decf
spysabbir <sabbirahammedsovon@gmail.com>
2023-12-12 10:17:07 +0600

    Update Sql

--- ecommerce-website.sql
+++ ecommerce-website.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 07, 2023 at 06:27 AM
+-- Generation Time: Dec 12, 2023 at 05:08 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
